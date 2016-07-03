089a497f0649d22a951b92a7106b6678093507fc
Dan Nichols <dan@nichols.works>
2016-07-03 02:44:57 -0400

    Spring Boot Skeleton

---
+++ src/main/java/works/nichols/media/WebConfig.java
@@ -0,0 +1,10 @@
+package works.nichols.media;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Log4j
+class WebConfig extends WebMvcConfigurerAdapter {
+}
